7f75b75309e5cbca7061a3effefd6fb9ce3474e1
Sara Oros Gochez <saraorosgochez@Saras-MacBook-Air.local>
2022-04-02 13:49:44 -0700

    reverted changes back to avoid error that appeared

--- models/Post.js
+++ models/Post.js
@@ -4,38 +4,38 @@ const sequelize = require("../config/connection");
 // create Post model
 class Post extends Model {}
 
-Post.init({
-  id: {
-    type: DataTypes.INTEGER,
-    allowNull: false,
-    primaryKey: true,
-    autoIncrement: true,
-  },
-  country_name: {
-    type: DataTypes.STRING,
-    allowNull: false,
-    primaryKey: true,
-  },
-  location: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-  user_id: {
-    type: DataTypes.INTEGER,
-    references: {
-      model: "user",
-      key: "id",
+Post.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
     },
-    text_content: {
+
+    country_name: {
       type: DataTypes.STRING,
       allowNull: false,
-      len: [1, 255],
+      primaryKey: true,
+    },
+    user_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: "user",
+        key: "id",
+      },
+    },
+    location: {
+      type: DataTypes.STRING,
+      allowNull: false,
     },
   },
-  sequelize,
-  freezeTableName: true,
-  underscored: true,
-  modelName: "post",
-});
+  {
+    sequelize,
+    freezeTableName: true,
+    underscored: true,
+    modelName: "post",
+  }
+);
 
 module.exports = Post;
