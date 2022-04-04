ae21a54a2b86018326f4bf97ba9da46ddcc01be4
Sonarie <sonarie1980@gmail.com>
2022-04-04 15:42:02 -0500

    everything is working

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -43,7 +43,17 @@ router.get("/", (req, res) => {
       posts = dbPostData.map((post) => post.get({ plain: true }));
       posts = posts.map(function (post) {
         if (post.location === "Brooklyn Park") {
-          post.location_color = "red";
+          post.location_color = "rgb(209,65,36)";
+        } else if (post.location === "HQ-Maple Plain") {
+          post.location_color = "rgb(229,114,0)";
+        } else if (post.location === "Nashua") {
+          post.location_color = "rgb(240,179,35)";
+        } else if (post.location === "Plymouth") {
+          post.location_color = "rgb(40, 114, 79)";
+        } else if (post.location === "Raleigh") {
+          post.location_color = "rgb(0, 85, 140)";
+        } else if (post.location === "Rosemount") {
+          post.location_color = "rgb(122, 92, 77)";
         } else {
           post.location_color = "black";
         }
