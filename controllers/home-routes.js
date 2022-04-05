69945c99cce630e53ccc4b22f8c243f1906e23c7
Paul Gottinger <pgottinger@outlook.com>
2022-04-05 12:59:49 -0500

    edit/delete/all eventspg

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -39,6 +39,51 @@ router.get("/", (req, res) => {
     });
 });
 
+router.get("/events", (req, res) => {
+  Post.findAll({
+    attributes: [
+      "id",
+      "user_id",
+      "created_at",
+      "country_name",
+      "location",
+      "description",
+    ],
+    include: [
+      {
+        model: Comment,
+        attributes: ["id", "comment", "post_id", "user_id", "created_at"],
+        include: {
+          model: User,
+          attributes: ["username"],
+        },
+      },
+      {
+        model: User,
+        attributes: ["username"],
+      },
+      //MAY NEED TO BE ADDED BACK IN LATER
+      // {
+      //   model: Country,
+      //   attributes: ["country_name"],
+      // },
+    ],
+  })
+    .then((dbPostData) => {
+      // pass a single post object into the homepage template
+      console.log(dbPostData[0]);
+      const posts = dbPostData.map((post) => post.get({ plain: true }));
+      res.render("all-events", {
+        posts,
+        loggedIn: req.session.loggedIn,
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
 router.get("/login", (req, res) => {
   if (req.session.loggedIn) {
     // place .sendMail() in here!
