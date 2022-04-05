7e67517c87d8c0b739393dd7ebc5b21c89431180
Sara Oros Gochez <saraorosgochez@Saras-MacBook-Air.local>
2022-04-04 22:04:12 -0700

    Changed Login button to Home and added another route to post and added Edit Post link, Link is't working

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,3 +1,5 @@
+// **** ALL EVENTS PAGE?? ****
+
 const router = require("express").Router();
 const { Post, User, Comment, Country } = require("./../models");
 
@@ -28,8 +30,55 @@ router.get("/", (req, res) => {
       // pass a single post object into the homepage template
       console.log(dbPostData[0]);
       const posts = dbPostData.map((post) => post.get({ plain: true }));
+      // THIS WOULD RENDER THE `ALL EVENTS PAGE` {i believe} ROUTES ARE NOT WORKING CORRECTLY:
       res.render("homepage", {
         posts,
+        loggedIn: req.session.loggedIn,
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+// ADD ON 4/4-SO get single post
+router.get("/post/:id", (req, res) => {
+  Post.findOne({
+    where: {
+      id: req.params.id,
+    },
+    attributes: ["id", "user_id", "created_at", "country_name", "location"],
+    include: [
+      {
+        model: Comment,
+        attributes: ["id", "comment", "post_id", "user_id", "created_at"],
+        include: {
+          model: User,
+          attributes: ["username"],
+        },
+      },
+      {
+        model: User,
+        attributes: ["username"],
+      },
+      //MAY NEED TO BE ADDED BACK IN LATER
+      // {
+      //   model: Country,
+      //   attributes: ["country_name"],
+      // },
+    ],
+  })
+    .then((dbPostData) => {
+      if (!dbPostData) {
+        res.status(404).json({ message: "No post found with this id" });
+        return;
+      }
+
+      const post = dbPostData.get({ plain: true });
+      // WE DO NOT HAVE A SINGLE-POST HANDLEBARS FILE
+      res.render("single-post", {
+        post,
         loggedIn: req.session.loggedIn,
       });
     })
