71468814c1039fd6352cdc9a9aaf8c35c5b04a31
Sara Oros Gochez <saraorosgochez@Saras-MacBook-Air.local>
2022-04-06 01:01:26 -0700

    Got rid of 'NaN' that would pop up in comment section, comments still aren't showing

--- public/javascript/comment.js
+++ public/javascript/comment.js
@@ -1,11 +1,13 @@
-async function commentFormHandler(event) {
+// async function commentFormHandler(event) {
   // event.preventDefault();
+
   // const comment_text = document
   //   .querySelector('textarea[name="comment-body"]')
   //   .value.trim();
   // const post_id = window.location.toString().split("/")[
   //   window.location.toString().split("/").length - 1
   // ];
+
   // if (comment_text) {
   //   const response = await fetch("/api/comments", {
   //     method: "POST",
@@ -17,16 +19,18 @@ async function commentFormHandler(event) {
   //       "Content-Type": "application/json",
   //     },
   //   });
+
   //   const result = await response.json();
   //   console.log(result);
+
   //   if (response.ok) {
   //     document.location.reload();
   //   } else {
   //     alert(result.errors[0].message);
   //   }
   // }
-}
+// }
 
-document
-  .querySelector(".comment-form")
-  .addEventListener("submit", commentFormHandler);
+// document
+//   .querySelector(".comment-form")
+//   .addEventListener("submit", commentFormHandler);
--- public/javascript/get-post.js
+++ public/javascript/get-post.js
@@ -2,7 +2,6 @@ async function fetchComments(event) {
 
     const post_id = window.location.toString().split("/")[window.location.toString().split("/").length - 1];
 
-  console.log("HEY");
   const response = await fetch(`/post/${post_id}`, {
     method: "GET",
     headers: {
